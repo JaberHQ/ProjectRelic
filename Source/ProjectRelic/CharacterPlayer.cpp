de044637ee12bbf1c2c4bcb5a4be8a80e79f78e3
JaberHQ <jaber4hmed@gmail.com>
2022-03-20 14:33:36 +0000

    More C++ Movement Code
    (JA)

--- Source/ProjectRelic/CharacterPlayer.cpp
+++ Source/ProjectRelic/CharacterPlayer.cpp
@@ -3,55 +3,80 @@
 
 #include "CharacterPlayer.h"
 
-ACharacterPlayer::ACharacterPlayer()
-{
-	AutoPossessPlayer = EAutoReceiveInput::Player0;
-
-	bUseControllerRotationYaw = false;
-
-	camera = CreateDefaultSubobject<UCameraComponent>( TEXT( "Camera" ) );
-	camera->AttachTo( RootComponent );
-	camera->SetRelativeLocation( FVector( 0, 0, 40 ) );
-
-}
-
-void ACharacterPlayer::SetupPlayerInputComponent( UInputComponent* PlayerInputComponent )
-{
-	PlayerInputComponent->BindAxis( "MoveForward", this, &ACharacterPlayer::moveForward );
-	PlayerInputComponent->BindAxis( "MoveRight", this, &ACharacterPlayer::moveRight );
-	PlayerInputComponent->BindAxis( "Turn", this, &ACharacterPlayer::turn );
-	PlayerInputComponent->BindAxis( "LookUp", this, &ACharacterPlayer::lookUp);
-}
-
 void ACharacterPlayer::moveForward( float inputAxis )
 {
-	if( inputAxis )
+	if( inputAxis != 0.0f )
 	{
+		// add movement in that direction
 		AddMovementInput( GetActorForwardVector(), inputAxis );
 	}
 }
 
 void ACharacterPlayer::moveRight( float inputAxis )
 {
-	if( inputAxis )
+	if( inputAxis != 0.0f )
 	{
+		// add movement in that direction
 		AddMovementInput( GetActorRightVector(), inputAxis );
 	}
 }
 
-void ACharacterPlayer::turn( float inputAxis )
+void ACharacterPlayer::turnRate( float rate )
 {
-	AddActorLocalRotation( FRotator( 0, inputAxis, 0 ) );
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput( rate * baseTurnRate * GetWorld()->GetDeltaSeconds() );
 }
 
-void ACharacterPlayer::lookUp( float inputAxis )
+void ACharacterPlayer::lookUpRate( float rate )
 {
-	if( inputAxis )
-	{
-		float temp = camera->GetRelativeRotation().Pitch + inputAxis;
-		if( temp <65 && temp > -65 )
-		{
-			camera->AddLocalRotation( FRotator( inputAxis, 0, 0 ) );
-		}
-	}
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput( rate * baseLookUpRate * GetWorld()->GetDeltaSeconds() );
 }
+
+ACharacterPlayer::ACharacterPlayer()
+{
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize( 55.f, 96.0f );
+	
+	// set our turn rates for input
+	baseTurnRate = 45.f;
+	baseLookUpRate = 45.f;
+
+	// Create a CameraComponent	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>( TEXT( "FirstPersonCamera" ) );
+	FirstPersonCameraComponent->SetupAttachment( GetCapsuleComponent() );
+	FirstPersonCameraComponent->SetRelativeLocation( FVector( -39.56f, 1.75f, 64.f ) ); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>( TEXT( "CharacterMesh1P" ) );
+	Mesh1P->SetOnlyOwnerSee( true );
+	Mesh1P->SetupAttachment( FirstPersonCameraComponent );
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->SetRelativeRotation( FRotator( 1.9f, -19.19f, 5.2f ) );
+	Mesh1P->SetRelativeLocation( FVector( -0.5f, -4.4f, -155.7f ) );
+}
+
+void ACharacterPlayer::SetupPlayerInputComponent( UInputComponent* PlayerInputComponent )
+{
+	Super::SetupPlayerInputComponent( PlayerInputComponent );
+
+	// set up gameplay key bindings
+	check( PlayerInputComponent );
+
+	// Bind jump events
+	PlayerInputComponent->BindAction( "Jump", IE_Pressed, this, &ACharacter::Jump );
+	PlayerInputComponent->BindAction( "Jump", IE_Released, this, &ACharacter::StopJumping );
+
+	// Bind movement events
+	PlayerInputComponent->BindAxis( "MoveForward", this, &ACharacterPlayer::moveForward );
+	PlayerInputComponent->BindAxis( "MoveRight", this, &ACharacterPlayer::moveRight );
+
+	PlayerInputComponent->BindAxis( "Turn", this, &APawn::AddControllerYawInput );
+	PlayerInputComponent->BindAxis( "TurnRate", this, &ACharacterPlayer::turnRate );
+	PlayerInputComponent->BindAxis( "LookUp", this, &APawn::AddControllerPitchInput );
+	PlayerInputComponent->BindAxis( "LookUpRate", this, &ACharacterPlayer::lookUpRate );
+}
+
+
--- Source/ProjectRelic/CharacterPlayer.h
+++ Source/ProjectRelic/CharacterPlayer.h
@@ -6,6 +6,7 @@
 #include "CharacterManager.h"
 #include "Components/InputComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "CharacterPlayer.generated.h"
 
 /**
@@ -16,27 +17,38 @@ class PROJECTRELIC_API ACharacterPlayer : public ACharacterManager
 {
 	GENERATED_BODY()
 
-private:
-	UPROPERTY( EditAnywhere, Category = "Camera" )
-		UCameraComponent* camera;
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+	UPROPERTY( VisibleDefaultsOnly, Category=Mesh )
+	USkeletalMeshComponent* Mesh1P;
 
+	/** First person camera */
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = ( AllowPrivateAccess = "true" ) )
+	UCameraComponent* FirstPersonCameraComponent;
+
+
+private:
 protected:
 	// Input 
 	void moveForward( float inputAxis );
 	void moveRight( float inputAxis );
-	void turn( float inputAxis );
-	void lookUp( float inputAxis );
-
-	// Sprint
-	void beginSprint();
-	void endSprint();
-
-	// Crouch
-	void beginCrouch();
-	void endCrouch();
+	void turnRate( float rate );
+	void lookUpRate( float rate );
 
 public:
 	ACharacterPlayer();
+
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = Camera )
+		float baseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = Camera )
+		float baseLookUpRate;
+
+	/** Returns Mesh1P subobject **/
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	/** Returns FirstPersonCameraComponent subobject **/
+	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent( class UInputComponent* PlayerInputComponent ) override;
