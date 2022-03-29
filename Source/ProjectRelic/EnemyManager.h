e288b2828ba59074661e18e05afa1f77917f5e95
JaberHQ <jaber4hmed@gmail.com>
2022-03-29 14:02:08 +0100

    Misc

---
+++ Source/ProjectRelic/EnemyManager.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterManager.h"
+#include "EnemyManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRELIC_API AEnemyManager : public ACharacterManager
+{
+	GENERATED_BODY()
+public:
+	AEnemyManager();
+};
