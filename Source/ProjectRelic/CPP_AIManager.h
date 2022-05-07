4cc84e082f0468cb61cbaf1de88e7768680fa89c
JaberHQ <jaber4hmed@gmail.com>
2022-05-07 14:45:12 +0100

    New AI Class

---
+++ Source/ProjectRelic/CPP_AIManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_CharacterManager.h"
+#include "CPP_AIManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRELIC_API ACPP_AIManager : public ACPP_CharacterManager
+{
+	GENERATED_BODY()
+	
+};
