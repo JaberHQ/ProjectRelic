4df42373dd911af6f48d2ce40bf99f6547efa2b2
JaberHQ <jaber4hmed@gmail.com>
2022-05-07 15:15:55 +0100

    Added New Ai Shooter Class

---
+++ Source/ProjectRelic/CPP_AssaultSoldier.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_AIManager.h"
+#include "CPP_AssaultSoldier.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRELIC_API ACPP_AssaultSoldier : public ACPP_AIManager
+{
+	GENERATED_BODY()
+	
+};
