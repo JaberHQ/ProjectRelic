498056918a4a753c2949a7d4c37ff0549032b7e6
JaberHQ <jaber4hmed@gmail.com>
2022-05-07 15:33:20 +0100

    Added Ai Controller

---
+++ Source/ProjectRelic/CPP_AIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "CPP_AIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRELIC_API ACPP_AIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
