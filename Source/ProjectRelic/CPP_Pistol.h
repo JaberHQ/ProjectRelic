f1d4229d9910777b7d965d001431e74cfb2c9bf4
JaberHQ <jaber4hmed@gmail.com>
2022-06-27 22:20:11 +0100

    Pistol

---
+++ Source/ProjectRelic/CPP_Pistol.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_GunManager.h"
+#include "CPP_Pistol.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRELIC_API ACPP_Pistol : public ACPP_GunManager
+{
+	GENERATED_BODY()
+	
+};
