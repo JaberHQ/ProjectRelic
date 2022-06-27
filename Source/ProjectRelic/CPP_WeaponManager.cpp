1b851885693301a518b00fac578d7f5be11ebcf0
JaberHQ <jaber4hmed@gmail.com>
2022-06-27 22:05:13 +0100

    weapon manager

---
+++ Source/ProjectRelic/CPP_WeaponManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_WeaponManager.h"
+
+// Sets default values
+ACPP_WeaponManager::ACPP_WeaponManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACPP_WeaponManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPP_WeaponManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
